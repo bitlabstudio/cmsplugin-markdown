ee73c99bee2564111dc0948eee7a359f651942a0
Damir Arbula <damir.arbula@gmail.com>
2015-10-02 15:44:03 +0200

    Rename tables to honor this change in django cms: http://django-cms.readthedocs.org/en/latest/upgrade/3.0.html#no-more-db-tablename-magic-for-plugins

---
+++ cmsplugin_markdown/migrations/0003_renametable.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+from cmsplugin_markdown.utils import rename_tables_new_to_old, rename_tables_old_to_new
+
+
+class Migration(SchemaMigration):
+
+    cms_plugin_table_mapping = (
+        # (old_name, new_name),
+        ('cmsplugin_markdownplugin', 'cmsplugin_markdown_markdownplugin'),
+    )
+
+    def forwards(self, orm):
+        rename_tables_old_to_new(db, self.cms_plugin_table_mapping)
+
+    def backwards(self, orm):
+        rename_tables_new_to_old(db, self.cms_plugin_table_mapping)
+
+    models = {
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'depth': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'numchild': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'path': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'cmsplugin_markdown.markdownplugin': {
+            'Meta': {'object_name': 'MarkdownPlugin', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
+            'markdown_text': ('django.db.models.fields.TextField', [], {'max_length': '8000'})
+        }
+    }
+
+    complete_apps = ['cmsplugin_markdown']
